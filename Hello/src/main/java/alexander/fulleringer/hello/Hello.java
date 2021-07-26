a67beab3a2492f76aed42df1a7422ae159e5aaff
Alexander Fulleringer <alexander.fulleringer@mail.mcgill.ca>
2021-07-26 14:44:49 -0400

    Update Hello.java

--- Hello/src/main/java/alexander/fulleringer/hello/Hello.java
+++ Hello/src/main/java/alexander/fulleringer/hello/Hello.java
@@ -1,16 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *@author Alexander Fulleringer
+ *email: alexander.fulleringer@mail.mcgill.ca
+ *date: 2021-07-26
+ *purpose: My many-eth Hello World program in an IDE
  */
 package alexander.fulleringer.hello;
 
 /**
- *
- * @author Alex
+
  */
 public class Hello {
     public static void main(String args[]){
         System.out.println("Hello World");
+        System.out.println("My name is Alex.");
     }
 }
